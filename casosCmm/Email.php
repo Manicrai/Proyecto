af4454bb8f7f93287ccbf56090069c142827884b
Marcos Sena <136614368+Manicrai@users.noreply.github.com>
2023-06-14 11:07:16 -0400

    Delete casosCmm directory

--- casosCmm/Email.php
+++
@@ -1,9 +0,0 @@
- <?php
-
-
-            $header = "From: noreply@example.com" . "\r\n";
-            $header. = "Reply-To: noreply@example.com" . "\r\n";
-            $header. = "X-Mailer: PHP/". phpversion();
-            $mail = mail($correo, "Nuevo Registro De Usuario", "Su Credenciales para iniciar sesion son las siguientes " $usuario "," $password)
-
- ?>
--- casosCmm/idsession.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	function guardar_id(){
-		$id = $_POST['bdid'];
-		$_SESSION['bdid'] = $id;
-		return  ;
-	}
-	guardar_id();
